33b9885dc322af72b02c890a800fd1e220f9d08a
Karthikeya <145262370+weasel-0@users.noreply.github.com>
2024-05-15 13:23:51 +0530

    modified css

--- src/components/Editor.svelte
+++ src/components/Editor.svelte
@@ -8,6 +8,7 @@
     import Typography from '@tiptap/extension-typography'
     import Placeholder from '@tiptap/extension-placeholder'
     import Document from '@tiptap/extension-document'
+
     import html2pdf from 'html2pdf.js'
     import { theme } from '$root/stores/themeStorage.js'
     import sunIcon from '$root/assets/sun-bold.svg'
@@ -47,9 +48,7 @@
                     placeholder: 'Write something …',
                 }),
             ],
-            // content: '<p>Hello World! 🌍️ </p>',
             onTransaction: () => {
-                // force re-render so `editor.isActive` works as expected
                 editor = editor
             },
         })
@@ -65,21 +64,21 @@
         const html = editor.getHTML()
         const elements = document.getElementsByClassName('tiptap')
         console.log(elements)
+
         const opt = {
             margin: [12, 12, 12, 12],
             filename: 'myfile.pdf',
             image: { type: 'jpeg', quality: 0.2 },
-            html2canvas: { scale: 2, useCORS: true },
+            html2canvas: { scale: 4, useCORS: true },
             jsPDF: { unit: 'mm', format: 'a4', orientation: 'portrait' },
             pagebreak: { mode: 'avoid-all', before: '#page2el' },
         }
 
         let combinedHtml = ''
         for (let i = 0; i < elements.length; i++) {
-            combinedHtml += elements[i].outerHTML
+            combinedHtml += `<div style="color: black;">${elements[i].outerHTML}</div>`
         }
 
-        // html2pdf(combinedHtml)
         html2pdf().set(opt).from(combinedHtml).save()
     }
 
@@ -114,7 +113,7 @@
             xmlns="http://www.w3.org/2000/svg"
             width="32"
             height="32"
-            fill="#000000"
+            fill="#444"
             viewBox="0 0 256 256"
             ><path
                 d="M200,164v8h12a12,12,0,0,1,0,24H200v12a12,12,0,0,1-24,0V152a12,12,0,0,1,12-12h32a12,12,0,0,1,0,24ZM92,172a32,32,0,0,1-32,32H56v4a12,12,0,0,1-24,0V152a12,12,0,0,1,12-12H60A32,32,0,0,1,92,172Zm-24,0a8,8,0,0,0-8-8H56v16h4A8,8,0,0,0,68,172Zm100,8a40,40,0,0,1-40,40H112a12,12,0,0,1-12-12V152a12,12,0,0,1,12-12h16A40,40,0,0,1,168,180Zm-24,0a16,16,0,0,0-16-16h-4v32h4A16,16,0,0,0,144,180ZM36,108V40A20,20,0,0,1,56,20h96a12,12,0,0,1,8.49,3.52l56,56A12,12,0,0,1,220,88v20a12,12,0,0,1-24,0v-4H148a12,12,0,0,1-12-12V44H60v64a12,12,0,0,1-24,0ZM160,57V80h23Z"
@@ -129,27 +128,28 @@
         /* color: #444; */
         font-size: 18px;
         margin: 40px auto;
-        /* max-width: 650px; */
         max-width: 980px;
     }
 
     .exportBtn {
-        background-color: #fff;
+        background-color: #dedede;
         border: 0;
         border-radius: 5px;
         position: fixed;
         bottom: 20px;
         right: 20px;
         padding: 8px;
+        transition: box-shadow 0.3s ease;
+        cursor: pointer;
     }
     .exportBtn:hover {
-        cursor: pointer;
-        background-color: #eee;
-        transition: 0.2s ease-in;
+        background-color: #acb5bd;
+        transition: 0.3s ease;
+        box-shadow: 0 0 2px rgba(0, 0, 0, 0.5);
     }
 
     .themeBtn {
-        background-color: #fff;
+        background-color: #dedede;
         border: 0;
         border-radius: 5px;
         position: fixed;
@@ -157,5 +157,10 @@
         right: 80px;
         padding: 8px;
         cursor: pointer;
+    }
+    .themeBtn:hover {
+        background-color: #acb5bd;
+        transition: 0.3s ease;
+        box-shadow: 0 0 2px rgba(0, 0, 0, 0.5);
     }
 </style>
